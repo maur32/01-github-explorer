5253e287c9a41fcf414d67edd9ee74474b02b74b
Mauricio <maur.martins32@gmail.com>
2021-07-12 21:35:21 -0300

    SASS adicionado

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,11 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 };
