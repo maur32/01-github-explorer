1b01ad0fc9d75e2e41472bcee876ee1d2f09e18a
Mauricio <maur.martins32@gmail.com>
2021-07-12 21:10:41 -0300

    Webpack Dev Server adicionado

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -10,6 +12,16 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
 
   module: {
     rules: [
